5249d69b53e47d4c2ecf6b639b8dcd61c2b6a2ed
Vivienne Nip <mangaluva.vivy@gmail.com>
2018-01-19 12:20:10 +0000

    First commit

---
+++ Assets/Paint Splatter/painterClick.cs
@@ -0,0 +1,29 @@
+﻿/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class painterClick : MonoBehaviour {
+
+	public Camera mainCamera;
+	public Texture2D splashTexture;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+		{
+			RaycastHit hit;
+			if (Physics.Raycast(mainCamera.transform.position, mainCamera.transform.forward, out hit))
+			{
+				//MyShader script = hit.collider.gameObject.GetComponent < MyShader;>();
+				//if (null != script)
+				//	script.PaintOn(hit.textureCoord, splashTexture);
+			}
+		}
+	}
+}
+*/
